63a40f4a5a87d3a3bf89eca5e316a2c1006c612a
miketako3 <28917343+miketako3@users.noreply.github.com>
2022-09-29 11:28:23 +0900

    zenn-markdown-htmlを組み込み

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,5 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import m2h from "zenn-markdown-html";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  return m2h(markdown)
 }
